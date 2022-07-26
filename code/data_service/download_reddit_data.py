6e5589264f5e42539209cc6a11ab6c0bd038ceac
root <root@DESKTOP-41RRSKT.localdomain>
2022-07-26 22:48:23 +0200

    Implemented download from reddit with failsafes

---
+++ code/data_service/download_reddit_data.py
@@ -0,0 +1,76 @@
+from enum import Enum, auto
+import datetime
+from dateutil.relativedelta import relativedelta
+from argparse import ArgumentParser
+from pathlib import Path
+import json
+import logging
+from typing import Tuple
+from pushshift_utils import scan_reddit_create_ds
+import praw
+
+# Enum to set a timespan
+class TimeUnits(Enum):
+    YEAR = auto()
+    MONTH = auto()
+    WEEK = auto()
+    DAY = auto()
+
+def get_timespan_upto(n_of : int, unit : TimeUnits) -> Tuple[datetime.datetime, datetime.datetime]:
+    now = datetime.datetime.today()
+    if unit == TimeUnits.YEAR:
+        delta = relativedelta(years=n_of)
+    elif unit == TimeUnits.MONTH:
+        delta = relativedelta(months=n_of)
+    elif unit == TimeUnits.WEEK:
+        delta = relativedelta(weeks=n_of)
+    else:
+        delta = relativedelta(days=n_of)
+    start = now - delta
+    return (start, now)
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+
+    # Read command line arguments
+
+    parser = ArgumentParser(description='Ingest image and post data from a set of subreddits')
+
+    parser.add_argument('n_of', metavar='N', nargs=1, type=int, help='The number of years/months/weeks/days up to which to get data')
+    parser.add_argument('unit', metavar='U', nargs=1,
+        choices=['years', 'months', 'weeks', "days"], help='Time unit to consider with N(years/months/weeks/days)')
+    parser.add_argument("location", metavar="L", type=Path, help="Where to save the obtained data", nargs="?", default=Path(__file__).parent)
+
+    args = vars(parser.parse_args())
+    simplify_list = lambda x : x[0] if isinstance(x, list) else x
+    args = {k:simplify_list(args[k]) for k in args}
+
+    unit_dict = {"years": TimeUnits.YEAR, "months": TimeUnits.MONTH, "weeks": TimeUnits.WEEK, "days": TimeUnits.DAY}
+
+    timespan = get_timespan_upto(args["n_of"], unit_dict[args["unit"]])
+
+    # Read settings
+    try:
+        with open(Path(__file__).parent / "data_extraction_settings.json", "rb") as f:
+            settings = json.loads(f.read())
+
+            try:
+                subreddits = settings["subreddits"]
+                to_extract = settings["to_extract"]
+                img_extensions = settings["img_extensions"]
+                pushshift_url = settings["pushshift_url"]
+
+                reddit_client_id = settings["reddit_client_id"]
+                reddit_client_secret = settings["reddit_client_secret"]
+            except KeyError:
+                logging.error("Settings file is malformed")
+                exit()
+    except FileNotFoundError:
+        logging.error("Settings file not found")
+    
+    # Instantiate praw (needed for galleries)
+    praw_connection = praw.Reddit(client_id=reddit_client_id,
+                         client_secret=reddit_client_secret, 
+                         user_agent="android:com.example.myredditapp:v1.2.3 (by u/kemitche)")
+                         
+    scan_reddit_create_ds(subreddits, to_extract, timespan, args["location"], img_extensions, pushshift_url, praw_connection)
--- code/data_service/pushshift_utils.py
+++ code/data_service/pushshift_utils.py
@@ -1,49 +1,65 @@
 import shutil
 import datetime
-import string
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import requests
 import logging
 import time
-import json
 from pathlib import Path
 import pandas as pd
+import praw
+from PIL import Image
+import io
 
-data_folder = Path(__file__).parent
-img_extensions = ["png", "jpg", "jpeg"]
-
-def get_raw_submissions(subreddits : List[str], date_start : datetime, date_end : datetime):
-    pushshift_url = "https://api.pushshift.io/reddit/search/submission/"
+def get_raw_submissions(subreddits : List[str], date_start : datetime, date_end : datetime, pushshift_url : str):
     # Build get request
     get_data = {}
     get_data["subreddit"] = ','.join(subreddits)
     get_data["before"] = int(time.mktime(date_end.timetuple()))
     get_data["after"] = int(time.mktime(date_start.timetuple()))
+    get_data["size"] = 100
 
+    aggregated = None
     # Send it
     try:
-        response = requests.get(pushshift_url, params=get_data)
+        # Get the timestamp for the last submission obtained
+        # Keep sending requests with increasing 'after'
+        while True:
+            response = requests.get(pushshift_url, params=get_data)
+            response.raise_for_status()
 
-        response.raise_for_status()
-        return response.json()
+            out = response.json()
+
+            if not out["data"]:
+                return aggregated
+            else:
+                if aggregated is None:
+                    aggregated = out
+                else:
+                    aggregated["data"] += out["data"]
+
+            last_timestamp = out["data"][-1]["created_utc"]
+            get_data["after"] = last_timestamp + 1 
+
     except requests.HTTPError as e:
         logging.error(f"HTTP error: {e}")
     return None
 
-def get_filtered_submissions(subreddits : List[str], date_start : datetime, date_end : datetime, clean_params : Dict):
-    raw_submissions = get_raw_submissions(subreddits, date_start, date_end)
+def get_filtered_submissions(subreddits : List[str], date_start : datetime, date_end : datetime, clean_params : Dict, pushshift_url : str):
+    raw_submissions = get_raw_submissions(subreddits, date_start, date_end, pushshift_url)
     if raw_submissions is None:
-        raise EnvironmentError(f"No submissions were obtained request: subreddit={subreddits}, date_start={date_start}, date_end={date_end}")
+        raise EnvironmentError(f"No submissions were obtained, Request: subreddit={subreddits}, date_start={date_start}, date_end={date_end}")
     raw_sub_list = raw_submissions["data"]
     filter_keys = lambda d : {k:d[k] for k in clean_params}
 
-    return map(filter_keys, raw_sub_list)
+    return list(map(filter_keys, raw_sub_list))
 
-def scan_reddit_create_ds(subreddits : List[str], clean_params : List[str]):
-    date_start = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-    date_end = date_start + datetime.timedelta(days=1)
+def scan_reddit_create_ds(subreddits : List[str], clean_params : List[str], 
+                            timespan : Tuple[datetime.datetime, datetime.datetime], data_folder : Path, img_extensions : List[str],
+                            pushshift_url : str, praw_connection : praw.Reddit):
+    date_start, date_end = timespan
 
-    submissions = get_filtered_submissions(subreddits=subreddits, date_start=date_start, date_end=date_end, clean_params=to_extract)
+    submissions = get_filtered_submissions(subreddits=subreddits, date_start=date_start, date_end=date_end, 
+                                            clean_params=clean_params, pushshift_url=pushshift_url)
 
     # Give a name to the record and create dir
     record_name = date_start.strftime("%Y-%m-%d") + "_" + date_end.strftime("%Y-%m-%d")
@@ -53,29 +69,48 @@ def scan_reddit_create_ds(subreddits : List[str], clean_params : List[str]):
     # Create dataframe
     df = pd.DataFrame(columns = clean_params)
     # Download images and add record to df
+    logging.info(f"Obtained {len(submissions)} submissions")
     for subm in submissions:
         img_url = subm["url"]
-        
-        url_extension = img_url[img_url.rfind(".")+1:]
-        if url_extension not in img_extensions:
-            logging.warning(f"Not considered : {img_url}")
-            continue 
+
+        # Take only the first image from a gallery (I don't trust galleries)
+        if "reddit.com/gallery/" in img_url:
+            submission = praw_connection.submission(url = img_url)
+            image_dict = submission.media_metadata
+            if image_dict is None:
+                continue
+            # Take the first image [0], the biggest resolution [s], the url [u]
+            img_url = list(image_dict.values())[0]['s']['u']
+            img_name = img_url[img_url.rfind("/") + 1:img_url.rfind("?")]
+        else:
+            url_extension = img_url[img_url.rfind(".")+1:]
+            if url_extension not in img_extensions:
+                logging.warning(f"Not considered : {img_url}")
+                continue 
+            img_name = img_url[img_url.rfind("/") + 1:]
+
         try:
             response = requests.get(img_url, stream=True)
-            img_name = img_url[img_url.rfind("/") + 1:]
-            with open(folder_path / img_name, 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-            del response
+            # If image size is less than 5kb skip it, it's either broken or too small
+            content = response.content
+            if len(content) < 5 * 1024:
+                continue
+            """
+            with open(, 'wb') as out_file:
+                shutil.copyfileobj(response.content, out_file)
+            """
+            image = Image.open(io.BytesIO(content))
+            image.save(folder_path / img_name)
 
+            del response
+            subm["url"] = img_url
             df = pd.concat([df, pd.DataFrame([subm])], ignore_index=True)
         except:
             logging.error(f"Could not download:{img_url}")
+    
+    df["created_utc"] = df["created_utc"].map(lambda x : datetime.datetime.fromtimestamp(x).isoformat())
+    logging.info(f"Of these {len(df)} were valid")
+
     df.to_csv(str(folder_path) + ".csv")
 
 
-with open(Path(__file__).parent / "data_extraction_settings.json", "rb") as f:
-    settings = json.loads(f.read())
-    subreddits = settings["subreddits"]
-    to_extract = settings["to_extract"]
-
-scan_reddit_create_ds(subreddits, to_extract)
